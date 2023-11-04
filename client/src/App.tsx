eecf2fcd7c41387d425897cf9003703c4194d581
Rafsan <rafsan.quayes@gmail.com>
2023-11-04 12:09:35 +0600

    Added navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { RouterProvider } from "react-router-dom";
 import './App.css'
 
 import router from './routes'
+import Navbar from "./components/navbar";
 
 function App() {
 
   return (
     <div className='background'>
+      <Navbar/>
       <RouterProvider router={router} />
     </div>
       
